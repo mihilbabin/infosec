7c9127d7c1fea61daf2bb12f7f1837d34ff993ac
Michael Babin <iamsuperman@ukr.net>
2018-09-26 18:40:19 +0300

    active class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def active_class?(test_path)
+    request.path == test_path ? 'active' : ''
+  end
 end
