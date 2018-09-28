8a766a8685be19ec28ed36742c8948572ab26868
Michael Babin <iamsuperman@ukr.net>
2018-09-28 11:16:04 +0300

    download title page link and file

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,8 @@
 class IndexController < ApplicationController
   def index
   end
+
+  def title_doc
+    send_file "#{Rails.root}/app/assets/docs/title.docx"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :admins, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'index#index'
+  get 'title_doc', to: 'index#title_doc'
 end
