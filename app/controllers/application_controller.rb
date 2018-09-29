1f75b64a6e74aec3c50afd971a6792bc38f9c9d7
Michael Babin <iamsuperman@ukr.net>
2018-09-29 12:07:10 +0300

    controller adjustment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :set_labs
-
   def set_labs
     @labs = Lab.order(created_at: :asc)
   end
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,7 @@
 class IndexController < ApplicationController
-  def index
-  end
+  before_action :set_labs, except: [:title_doc]
+
+  def index; end
 
   def title_doc
     send_file "#{Rails.root}/app/assets/docs/title.docx"
