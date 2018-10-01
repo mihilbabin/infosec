08a972126a5c41825c1f5ea5e5d7fcfa349467d5
Michael Babin <iamsuperman@ukr.net>
2018-10-01 14:54:36 +0300

    adjust controller

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -1,12 +1,15 @@
 class LabsController < ApplicationController
-  before_action :set_labs
+  before_action :set_labs, except: :download
+  before_action :set_lab
 
-  def show
-    @lab = Lab.find params[:id]
-  end
+  def show; end
 
   def download
-    
   end
-  
+
+  private
+
+  def set_lab
+    @lab = Lab.find params[:id]
+  end  
 end
