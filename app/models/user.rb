9d93ca1cbd057a98bf92401448b9a5c200fb9599
Michael Babin <iamsuperman@ukr.net>
2018-10-11 21:31:32 +0300

    email field adjusted

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, :email, :password, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 10, maximum: 32 }
 
   scope :locked, -> { where('attempts >= ?', MAX_ATTEMPTS) }
+
+  before_save { self.email = email.downcase }
 
   def locked?
     attempts >= MAX_ATTEMPTS
