2440e0b36a9b558b189dab473a705b758c8238fe
Michael Babin <iamsuperman@ukr.net>
2018-10-11 21:07:45 +0300

    user model validations and stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  MAX_ATTEMPTS = 7
+  has_secure_password
+  validates :name, :email, :password, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 10, maximum: 32 }
+
+  def locked?
+    attempts >= MAX_ATTEMPTS
+  end
 end
