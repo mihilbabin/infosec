c7d8efb3549582558ac34cab2fab8cf717d4f2bd
Michael Babin <iamsuperman@ukr.net>
2018-10-11 21:51:08 +0300

    added registration page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_labs
+
   def new
     @user = User.new
   end
@@ -7,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      flash[:notice] = I18n.t('users.registrations.success')
+      flash[:notice] = I18n.t('users.registrations.success', name: @user.name)
       redirect_to root_url
     else
       flash.now.alert = I18n.t('users.registrations.error')
