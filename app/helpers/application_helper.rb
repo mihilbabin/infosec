5df68995403d5a7a4ca0d9852b961ef9b2f0659c
Michael Babin <iamsuperman@ukr.net>
2018-10-12 09:05:02 +0300

    bootstrap & translations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,9 +11,16 @@ module ApplicationHelper
     case level
     when :notice then 'alert alert-info'
     when :success then 'alert alert-success'
-    when :error then 'alert alert-error'
-    when :alert then 'alert alert-error'
+    when :error then 'alert alert-danger'
+    when :alert then 'alert alert-danger'
     end
   end
 
+  def errors_for(model, field)
+    if model.errors[field].any?
+      content_tag(:div, class: 'invalid-feedback') do
+        model.errors[field].first
+      end
+    end
+  end
 end
