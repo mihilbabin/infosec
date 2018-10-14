8527e9c33c691a31e2209d937a986fe1463221c2
Michael Babin <iamsuperman@ukr.net>
2018-10-14 11:17:20 +0300

    added lock logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
   before_action :set_labs, except: :destroy
+  before_action :set_user, only: :create
 
   def new
     redirect_to root_url if logged_in?
   end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate(params[:session][:password])
-      log_in user
-      redirect_to root_url
+    if @user&.authenticate(params[:session][:password])
+      reject_locked!
     else
+      @user&.increment!(:attempts)
       flash.now[:error] = I18n.t('users.sessions.error')
       render :new
     end
@@ -22,4 +22,17 @@ class SessionsController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find_by email: params[:session][:email].downcase
+  end
+
+  def reject_locked!
+    if @user.locked?
+      redirect_to new_session_path, alert: I18n.t('users.sessions.locked')
+    else
+      log_in @user
+      redirect_to root_url, success: I18n.t('users.sessions.success', name: @user.name)
+    end
+  end
 end
