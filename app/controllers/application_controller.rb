c553aec94c301b3354e08ed4647c7b205a9b0981
Michael Babin <iamsuperman@ukr.net>
2018-10-14 10:46:19 +0300

    helpers included

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
   def set_labs
     @labs = Lab.order(created_at: :asc)
   end
